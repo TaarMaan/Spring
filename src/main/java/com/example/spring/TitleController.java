310d7bfe7474cd3488cec8510402e0e9d70fa359
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-03-17 15:01:54 +0300

    Initial commit

---
+++ src/main/java/com/example/spring/TitleController.java
@@ -0,0 +1,10 @@
+package com.example.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TitleController {
+
+}
